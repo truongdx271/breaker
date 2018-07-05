37665e99ae1e69e2fa6fee88b92e312e36fc21d8
truongdx271 <truongdinh271@gmail.com>
2018-07-05 21:35:43 +0700

    string format

--- oceanBank.py
+++ oceanBank.py
@@ -13,7 +13,8 @@ def upvote(ms):
     sessionId = r.headers['Set-Cookie'].split(';')[0].split('=')[1]
     userId = r.headers['Set-Cookie'].split(';')[4].split('=')[1]
     # set url to download captcha
-    urlCaptcha = "http://hoidienvannghe.oceanbank.vn/GetCapchar.aspx?user_id="+userId+"&ms="+ms
+    urlCaptcha = "http://hoidienvannghe.oceanbank.vn/GetCapchar.aspx?user_id=" + \
+        userId+"&ms="+str(ms)
     # download captcha file
     capImg = "captcha"+str(ms)+".jpg"
     f = open(capImg, 'wb')
@@ -24,7 +25,7 @@ def upvote(ms):
     captcha = text.split(',')[0]
     # submit vote
     urlUpvote = "http://hoidienvannghe.oceanbank.vn/UpdateVote.aspx?user_id=" + \
-        userId + "&vp_id="+ms+"&capchar=" + captcha
+        userId + "&vp_id="+str(ms)+"&capchar=" + captcha
     re = requests.get(urlUpvote)
     print re.content
 
