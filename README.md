7221bb9802dcfe539ff9b30d92e51f1de35494a9
root <root@localhost.localdomain>
2018-07-05 09:22:32 +0700

    change code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # breaker
+
+### 1. Install require lib
+###   pip install +lib+
+
+### 2. apt-get install tesseract-ocr
+
+### 3. Use:
+###   python oceanBank.py -> Done
--- oceanBank.py
+++ oceanBank.py
@@ -2,31 +2,30 @@ import requests
 import urllib
 from PIL import Image
 import pytesseract
+import time
 
-urlIndex = "http://hoidienvannghe.oceanbank.vn/Binhchon.aspx?location=MB"
+def upvote():
+    urlIndex = "http://hoidienvannghe.oceanbank.vn/Binhchon.aspx?location=MB"
+    # get header -> userid, sessionid
+    r = requests.get(urlIndex)
+    sessionId = r.headers['Set-Cookie'].split(';')[0].split('=')[1]
+    userId = r.headers['Set-Cookie'].split(';')[4].split('=')[1]
+    # set url to download captcha
+    urlCaptcha = "http://hoidienvannghe.oceanbank.vn/GetCapchar.aspx?user_id="+userId+"&ms=121"
+    # download captcha file
+    f = open('captcha.jpg', 'wb')
+    f.write(urllib.urlopen(urlCaptcha).read())
+    f.close()
+    # read captcha
+    text = pytesseract.image_to_string(Image.open('captcha.jpg'))
+    captcha = text.split(',')[0]
+    # submit vote
+    urlUpvote = "http://hoidienvannghe.oceanbank.vn/UpdateVote.aspx?user_id=" + \
+        userId + "&vp_id=121&capchar=" + captcha
+    re = requests.get(urlUpvote)
+    print re.content
 
-# get header -> userid, sessionid
-r = requests.get(urlIndex)
-sessionId = r.headers['Set-Cookie'].split(';')[0].split('=')[1]
-userId = r.headers['Set-Cookie'].split(';')[4].split('=')[1]
-
-# set url to download captcha
-urlCaptcha = "http://hoidienvannghe.oceanbank.vn/GetCapchar.aspx?user_id="+userId+"&ms=121"
-print sessionId
-print urlCaptcha
-
-# download captcha file
-f = open('captcha.jpg', 'wb')
-f.write(urllib.urlopen(urlCaptcha).read())
-f.close()
-
-# read captcha
-text = pytesseract.image_to_string(Image.open('captcha.jpg'))
-captcha = text.split(',')[0]
-
-# submit vote
-urlUpvote = "http://hoidienvannghe.oceanbank.vn/UpdateVote.aspx?user_id=" + \
-    userId + "&vp_id=121&capchar=" + captcha
-
-re = requests.get(urlUpvote)
-print re.content
+if __name__=="__main__":
+    while True:
+        time.sleep(10)
+        upvote()
