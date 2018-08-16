d9f6373a9e5dc9d2c9330159e7c03e1b2f7ac949
cyberjunky <idiscr@gmail.com>
2018-08-16 18:19:49 +0000

    new ocean

--- oceanBank.py
+++ oceanBank.py
@@ -4,6 +4,26 @@ from PIL import Image
 import pytesseract
 import time
 from random import randint
+from faker import Factory
+
+fake_generator = Factory.create()
+
+def get_random_ip():
+    return fake_generator.ipv4()
+
+def set_header(ip,sessionId):
+    return {
+    "Host": "hoidienvannghe.oceanbank.vn",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+    "Accept": "*/*",
+    "Referer": "http://hoidienvannghe.oceanbank.vn/Binhchon.aspx?location=MB",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "X-Forwarded-For": ip,
+    "X-Requested-With": "XMLHttpRequest",
+    "Cookie": "ASP.NET_SessionId="+sessionId,
+    "Connection": "close"
+    }
 
 
 def upvote(ms):
@@ -22,18 +42,19 @@ def upvote(ms):
     f.close()
     # read captcha
     text = pytesseract.image_to_string(Image.open(capImg))
-    captcha = text.split(',')[0]
+    captcha = text.replace('\'','').replace(',','').replace('s','5').replace('.','').replace(' ','')
+    print captcha
     # submit vote
     urlUpvote = "http://hoidienvannghe.oceanbank.vn/UpdateVote.aspx?user_id=" + \
         userId + "&vp_id="+str(ms)+"&capchar=" + captcha
-    re = requests.get(urlUpvote)
-    print re.content
+    re = requests.get(urlUpvote,headers=set_header(get_random_ip(),sessionId))
+    print re.status_code
 
 
 if __name__ == "__main__":
-    while True:
-        upvote(121)
-        upvote(120)
-        sleeptime = (randint(10, 20))
-        time.sleep(sleeptime)
-        print "Sleep for: "+str(sleeptime)
+    #while True:
+    #    upvote(121)
+    #    sleeptime = (randint(10, 20))
+    #    time.sleep(sleeptime)
+    #    print "Sleep for: "+str(sleeptime)
+    upvote(121)
